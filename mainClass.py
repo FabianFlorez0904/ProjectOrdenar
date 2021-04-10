48e3199a14bc7b80638aab26549c1c56cba8cec3
Fabian Dario Florez Raigoso <fabflorez@gmail.com>
2021-04-10 08:11:25 -0500

    Se prueba un fileChosser en python

--- mainClass.py
+++ mainClass.py
@@ -2,9 +2,11 @@
 # * Author: Fabian Dario Florez Raigoso
 # *
 from ordenarClass import * 
+from tkinter.filedialog import askopenfilename
 
 def main():
     print("Hola Mundo!")
-
+    filename = askopenfilename()
+    print(filename)
 # Ejecutamos el main
 main()
